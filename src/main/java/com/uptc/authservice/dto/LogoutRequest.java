0036876b31f5f04cf4bcab9ca3e385ceb0087c63
Nicolás Munévar Gómez <90329083+HK-Kapyushon@users.noreply.github.com>
2025-10-20 23:23:25 -0500

    feat(QUEJAS-76): cerrar sesion (#5)
    Crear endpoint: POST /auth/logout
    Marca la sesión como inactiva o la elimina.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/com/uptc/authservice/dto/LogoutRequest.java
@@ -0,0 +1,8 @@
+package com.uptc.authservice.dto;
+
+public class LogoutRequest {
+    private String token;
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+}
